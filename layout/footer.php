baaef15bfd7ffa53c2c7ed1b2f63ff66da073d80
Noah <n.guilletgrimaud@ecoles-wis.net>
2021-02-02 16:20:28 +0100

    update : home / footer

--- layout/footer.php
+++ layout/footer.php
@@ -32,6 +32,10 @@
                     </li>
                 </ul>
             </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
             <!--Grid column-->
         </div>
         <!--Grid row-->
