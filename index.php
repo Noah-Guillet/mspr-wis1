2a6c97bdf217109189e94d1c87d116ed0184d091
Noah <n.guilletgrimaud@ecoles-wis.net>
2021-02-02 12:48:49 +0100

    update : img home

--- index.php
+++ index.php
@@ -1,6 +1,74 @@
 <?php require_once 'layout/header.php' ?>
 
 <main>
+
+    <section id="home-entete">
+
+        <div class="container">
+
+            <div class="row align-items-start">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+            <div class="row align-items-end">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section-cards>
+
+        <div class="container">
+
+            <div class="row align-items-start">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+            <div class="row align-items-end">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+            </div>
+
+        </div>
+
+
+    </section-cards>
+
 </main>
 
 <?php require_once 'layout/footer.php' ?>
