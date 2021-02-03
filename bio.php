fc5164bf255ee8c0b4f073b7cb90ec1c5abf8a0f
Kelvin <fabert.kelvin@gmail.com>
2021-02-03 16:16:31 +0100

    update bio

--- bio.php
+++ bio.php
@@ -2,67 +2,132 @@
 
 <main>
     <section id="bio-jumbotron">
-        <!--<div class="container" style="background: no-repeat url('image/jumb-bio.jpg'); height: 400px"> -->
+        <div class="container"
+             style="background: no-repeat url('image/fond-4k.jpg'); background-size: cover; height: 700px; min-width: 100%">
             <h1>Protection contre l'environnement</h1>
+        </div>
     </section>
 
 
     <section id="bio-cards">
-    <div class="container">
-        <div class="row align-items-start">
-            <div class="col">
-                <div class="card card-bio" style="width: 18rem;">
-                    <img src="image/bep.png" class="card-img-top" alt="#">
-                    <div class="card-body">
-                        <p class="card-text">Sur les 89 milliards de bouteilles en plastique,
-                            seulement 20% d’entre elles étaient recyclées. Une grande partie finissent malheureusement dans
-                            les océans</p>
+        <div class="container">
+            <div class="row align-items-start">
+                <div class="col">
+                    <div class="card card-bio" style="width: 18rem;">
+                        <img src="image/bep.png" class="card-img-top" alt="#">
+                        <div class="card-body">
+                            <p class="card-text">Sur les 89 milliards de bouteilles en plastique,
+                                seulement 20% d’entre elles étaient recyclées. Une grande partie finissent
+                                malheureusement dans
+                                les océans</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card card-bio" style="width: 18rem;">
-                    <img src="image/pollution.jpg" class="card-img-top" alt="#">
-                    <div class="card-body">
-                        <p class="card-text">Entièrement comestible et biodégradable, cette nouvelle forme de packaging a
-                            pour but de réduire le nombre de déchets plastiques ainsi que les coûts de production. Se
-                            présentant alors comme une véritable solution alternative aux bouteilles d’eau omniprésentes
-                            dans nos rayons.
-                        </p>
+                <div class="col">
+                    <div class="card card-bio" style="width: 18rem;">
+                        <img src="image/pollution.jpg" class="card-img-top" alt="#">
+                        <div class="card-body">
+                            <p class="card-text">Entièrement comestible et biodégradable, cette nouvelle forme de
+                                packaging a
+                                pour but de réduire le nombre de déchets plastiques ainsi que les coûts de production.
+                                Se
+                                présentant alors comme une véritable solution alternative aux bouteilles d’eau
+                                omniprésentes
+                                dans nos rayons.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card card-bio" style="width: 18rem;">
-                    <img src="image/eau-capsule.jpg" class="card-img-top" alt="#">
-                    <div class="card-body">
-                        <p class="card-text">Constitué d’une double membrane sphérique produite à partir d’algues brunes et
-                            de chlorure de calcium qui protège l’intérieur de manière hygiénique.
-                            Elle se transporte, se vide et puis se mange ou se jette sans impact sur l’environnement.
-                        </p>
+                <div class="col">
+                    <div class="card card-bio" style="width: 18rem;">
+                        <img src="image/eau-capsule.jpg" class="card-img-top" alt="#">
+                        <div class="card-body">
+                            <p class="card-text">Constitué d’une double membrane sphérique produite à partir d’algues
+                                brunes et
+                                de chlorure de calcium qui protège l’intérieur de manière hygiénique.
+                                Elle se transporte, se vide et puis se mange ou se jette sans impact sur
+                                l’environnement.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    </section>
-    
-    <section id="bio-img">
-        <img src="image/Pollution-1.jpg">
     </section>
 
-    <section id="bio-txt">
+    <section id="bio-article">
+                    <div class="container">
+                        <p>
+                            Composante essentielle de tous les êtres vivants, l’eau est aussi un milieu de vie pour certains d’entre
+                            eux. Elle circule sans cesse, des précipitations vers les eaux de surface (cours d’eau, rivières,
+                            fleuves,
+                            lacs, etc.), la végétation (sauvage et agricole), les eaux souterraines (nappes), les mers et les
+                            océans.
+                            Sous l’effet des activités humaines, elle fait l’objet de nombreuses pollutions susceptibles d’affecter
+                            la
+                            biodiversité et la santé humaine. Protéger la ressource en eau constitue donc un enjeu majeur. Le
+                            Service
+                            des données et études statistiques (SDES) et l’Office français de la biodiversité (OFB) s’inscrivent
+                            dans
+                            cette démarche en informant et sensibilisant régulièrement l’ensemble des acteurs sur les enjeux liés à
+                            la
+                            préservation de l’eau et des milieux aquatiques.
+                        </p>
+                    </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section id="bio-article">
         <div class="container">
-        <p>
-            Composante essentielle de tous les êtres vivants, l’eau est aussi un milieu de vie pour certains d’entre
-            eux. Elle circule sans cesse, des précipitations vers les eaux de surface (cours d’eau, rivières, fleuves,
-            lacs, etc.), la végétation (sauvage et agricole), les eaux souterraines (nappes), les mers et les océans.
-            Sous l’effet des activités humaines, elle fait l’objet de nombreuses pollutions susceptibles d’affecter la
-            biodiversité et la santé humaine. Protéger la ressource en eau constitue donc un enjeu majeur. Le Service
-            des données et études statistiques (SDES) et l’Office français de la biodiversité (OFB) s’inscrivent dans
-            cette démarche en informant et sensibilisant régulièrement l’ensemble des acteurs sur les enjeux liés à la
-            préservation de l’eau et des milieux aquatiques.
-        </p>
+            <p>
+                Composante essentielle de tous les êtres vivants, l’eau est aussi un milieu de vie pour certains d’entre
+                eux. Elle circule sans cesse, des précipitations vers les eaux de surface (cours d’eau, rivières,
+                fleuves,
+                lacs, etc.), la végétation (sauvage et agricole), les eaux souterraines (nappes), les mers et les
+                océans.
+                Sous l’effet des activités humaines, elle fait l’objet de nombreuses pollutions susceptibles d’affecter
+                la
+                biodiversité et la santé humaine. Protéger la ressource en eau constitue donc un enjeu majeur. Le
+                Service
+                des données et études statistiques (SDES) et l’Office français de la biodiversité (OFB) s’inscrivent
+                dans
+                cette démarche en informant et sensibilisant régulièrement l’ensemble des acteurs sur les enjeux liés à
+                la
+                préservation de l’eau et des milieux aquatiques.
+            </p>
+        </div>
+        <div>
+            <img src="image/Pollution-1.jpg">
         </div>
     </section>
 </main>
