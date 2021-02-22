4d13d628e23b6276768aec9b8508351d02ffb88b
Noah <n.guilletgrimaud@ecoles-wis.net>
2021-02-22 17:47:59 +0100

    update : techno

--- techno.php
+++ techno.php
@@ -12,31 +12,37 @@
                 <div class="col1">
                     <div class="row align-items-start">
                         <div class="col">
-                            <img src="image/algues.jpg" width="800" height="600">
+                            <img src="image/algues.jpg" width="600" height="400">
                         </div>
                         <div class="col">
+                            <div class="tech-texte1">
                             <p class="card-text">L’eau est d'abord congelée sous forme de bulle puis trempée dans de la gélatine d’algues, gélatine qui bloque l’eau quand la glace fond.</p>
-                        </div>
+                            </div>
+                            </div>
                     </div>
                 </div>
                 <div class="col2">
                     <div class="row align-items-center">
                         <div class="col">
+                            <div class="tech-texte2">
                             <p class="card-text"> Il est tout à fait possible d’enfermer d’autres boissons dans la capsule, coca, bière, vin… Elle contenait 4 cl d’eau, aujourd’hui les bulles ont différents volumes !</p>
-                        </div>
+                            </div>
+                            </div>
                         <div class="col">
-                            <img src="image/boissons.jpg">
+                            <img src="image/boissons.jpg" >
                         </div>
                     </div>
                 </div>
                 <div class="col3">
                     <div class="row align-items-end">
                         <div class="col">
-                            <img src="image/eau-capsule.jpg">
+                            <img src="image/eau-capsule.jpg" width="600" height="400">
                         </div>
                         <div class="col">
+                            <div class="tech-texte3">
                             <p class="card-text">Pour boire cette capsule, plusieurs possibilités : faire un trou dans la membrane et recueillir le précieux liquide. Ou simplement avaler la bulle d'eau toute entière, l'enveloppe étant comestible grâce à sa fabrication à base d'algues. Enfin, il est toujours possible de retirer, comme la peau d'un fruit, la fine pellicule qui peut ensuite servir de compost.</p>
-                        </div>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div>
